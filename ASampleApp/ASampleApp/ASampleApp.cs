1e06934bbc8174fcdd41c9e17df0399de49acf8a
ac <andrew.chung@xamarin.com>
2017-07-09 08:09:30 -0700

    removed unneeded comments

---
+++ ASampleApp/ASampleApp/ASampleApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ASampleApp.Data;
+using Xamarin.Forms;
+
+namespace ASampleApp
+{
+	public class App : Application
+	{
+
+        public static DogRepository DogRep { get; private set; }
+
+		public App ()
+		{
+
+            string dbPath = FileAccessHelper.GetLocalFilePath("dog3.db3");
+            DogRep = new DogRepository(dbPath);
+
+			var applicationStartPage = new FirstPage ();
+
+			//// The root page of your application
+			//var content = new ContentPage {
+			//	Title = "ASampleApp",
+			//	Content = new StackLayout {
+			//		VerticalOptions = LayoutOptions.Center,
+			//		Children = {
+			//			new Label {
+			//				HorizontalTextAlignment = TextAlignment.Center,
+			//				Text = "Welcome to Xamarin Forms!"
+			//			}
+			//		}
+			//	}
+			//};
+
+			MainPage = new NavigationPage (applicationStartPage);
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ ASampleApp/ASampleApp/Pages/DogListPage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using ASampleApp.Models;
+using Xamarin.Forms;
+namespace ASampleApp
+{
+    public class DogListPage : BaseContentPage<DogListViewModel> //: BaseContentPage<DogListViewModel>
+    {
+
+        ListView _dogListView;
+
+        public DogListPage()
+        {
+            _dogListView = new ListView();
+
+            var myTemplate = new DataTemplate(typeof(TextCell));
+
+            _dogListView.ItemTemplate = myTemplate;
+
+            var _myListOfDogs = App.DogRep.GetAllDogs();
+            _dogListView.ItemsSource = _myListOfDogs;
+
+
+
+
+            myTemplate.SetBinding(TextCell.TextProperty, "Name");
+            myTemplate.SetBinding(TextCell.DetailProperty, "FurColor");
+
+            Content = new StackLayout()
+            {
+                Children = {
+                    _dogListView
+                }
+
+            };
+
+
+        }
+    }
+
+	public class DogViewCell : ViewCell
+	{
+        public DogViewCell()
+        {
+
+
+            var moreAction = new MenuItem { Text = "More" };
+            moreAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+            moreAction.Clicked += async (sender, e) =>
+            {
+                var mi = ((MenuItem)sender);
+                Debug.WriteLine("More Context Action clicked: " + mi.CommandParameter);
+            };
+
+            var deleteAction = new MenuItem { Text = "Delete", IsDestructive = true }; // red background
+            deleteAction.SetBinding(MenuItem.CommandParameterProperty, new Binding("."));
+            deleteAction.Clicked += async (sender, e) =>
+            {
+                var mi = ((MenuItem)sender);
+                Debug.WriteLine("Delete Context Action clicked: " + mi.CommandParameter);
+            };
+            // add to the ViewCell's ContextActions property
+            ContextActions.Add(moreAction);
+            ContextActions.Add(deleteAction);
+        }
+    }
+
+
+
+
+
+
+}
